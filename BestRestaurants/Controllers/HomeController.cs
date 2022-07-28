45627d764b5bcccf5262d97019d5bcb801fb863b
Sandra Tena <sandratena06@gmail.com>
2022-07-28 14:12:05 -0700

    Add code to cuisine, restaurants, best restaurants models and cuisines controllers
    Co-authored-by: Grace Kostanich <fl5812597@gmail.com>

---
+++ BestRestaurants/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestRestaurants.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    // [Route("/favorite_photos")]
+    // public ActionResult FavoritePhotos()
+    // {
+    //   return View();
+    // }
+  }
+}
